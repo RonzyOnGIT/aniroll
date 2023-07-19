11e45f5d97e61d1f72d5d499b0265ca067678d2f
JohnnyBoi <ninjakai25@gmail.com>
2023-07-19 16:56:05 -0500

    add carousel for hero component

---
+++ src/components/circle/Circle.jsx
@@ -0,0 +1,9 @@
+
+export const Circle = () => {
+
+    return (
+        <>
+        
+        </>
+    )
+}
--- src/features/animes/HeroAnimes.jsx
+++ src/features/animes/HeroAnimes.jsx
@@ -7,32 +7,37 @@ export const HeroAnimes = () => {
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    useEffect(() => {
-        setTimeout(() => {
-            if (currentIndex > 3) {
-                setCurrentIndex(0);
-                return;
-            }
-            setCurrentIndex(prev => prev + 1);
-        }, 4000)
-    })
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         if (currentIndex === 3) {
+    //             setCurrentIndex(0);
+    //             return;
+    //         }
+    //         setCurrentIndex(prev => prev + 1);
+    //     }, 4000)
+    // })
+
+    // const changeIndex = () => {
+
+
+    // }
 
     return (
         <>
-            <div className={styles.backgroundImage}>
+            <div className={styles[`backgroundImage${currentIndex}`]}>
                 <div className={styles.content}>
                     <h2>Hell's Paradise</h2>
                     <p>Set in the Edo period of Japan, it follows the ninja Gabimaru and the executioner Yamada Asaemon Sagiri as they search for the elixir of immortality.</p>
                     <button>Watch Now</button>
                 </div>
-            </div>
-            <div className={styles.backgroundImagejjk}>
-                <div className={styles.content}>
-                    <h2>Hell's Paradise</h2>
-                    <p>Set in the Edo period of Japan, it follows the ninja Gabimaru and the executioner Yamada Asaemon Sagiri as they search for the elixir of immortality.</p>
-                    <button>Watch Now</button>
+                <div className={styles.circleContainer}>
+                    <div className={styles.circle}></div>
+                    <div className={styles.circle}></div>
+                    <div className={styles.circle}></div>
+                    <div className={styles.circle}></div>
                 </div>
             </div>
+
         </>
     )
 }
