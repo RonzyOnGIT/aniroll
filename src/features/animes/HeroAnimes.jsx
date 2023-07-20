916ad21739092c5581ffaf5c856798cc13028971
JohnnyBoi <ninjakai25@gmail.com>
2023-07-19 20:16:12 -0500

    add circle to switch hero anime

--- src/features/animes/HeroAnimes.jsx
+++ src/features/animes/HeroAnimes.jsx
@@ -7,20 +7,23 @@ export const HeroAnimes = () => {
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         if (currentIndex === 3) {
-    //             setCurrentIndex(0);
-    //             return;
-    //         }
-    //         setCurrentIndex(prev => prev + 1);
-    //     }, 4000)
-    // })
-
-    // const changeIndex = () => {
+    useEffect(() => {
 
 
-    // }
+        setTimeout(() => {
+
+            if (currentIndex === 3 || currentIndex > 3) {
+                setCurrentIndex(0);
+                return;
+            }
+            setCurrentIndex(prev => prev + 1);
+        }, 4000)
+    })
+
+    const changeIndex = (index) => {
+        setCurrentIndex(index);
+    }
+
 
     return (
         <>
@@ -31,11 +34,12 @@ export const HeroAnimes = () => {
                     <button>Watch Now</button>
                 </div>
                 <div className={styles.circleContainer}>
-                    <div className={styles.circle}></div>
-                    <div className={styles.circle}></div>
-                    <div className={styles.circle}></div>
-                    <div className={styles.circle}></div>
+                    <div className={currentIndex === 0 ? styles.circleActive : styles.circle} onClick={() => changeIndex(0)}></div>
+                    <div className={currentIndex === 1 ? styles.circleActive : styles.circle} onClick={() => changeIndex(1)}></div>
+                    <div className={currentIndex === 2 ? styles.circleActive : styles.circle} onClick={() => changeIndex(2)}></div>
+                    <div className={currentIndex === 3 ? styles.circleActive : styles.circle} onClick={() => changeIndex(3)}></div>
                 </div>
+                <h1 id={styles.debug}>{currentIndex}</h1>
             </div>
 
         </>
