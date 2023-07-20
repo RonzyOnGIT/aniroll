85b5bb1207fd87ee3eb33e2eb9e88f43897ff7c8
Ronzy <ninjakai25@gmail.com>
2023-07-20 11:48:36 -0500

    add data for hero backgrounds

---
+++ src/data/heroData.js
@@ -0,0 +1,27 @@
+import hell from '../images/backgrounds/hellparadise-min.png';
+import jjk from '../images/backgrounds/jjkseason2-min.png';
+import kenshin from '../images/backgrounds/kenshin-min.png';
+import tyb from '../images/backgrounds/tyb-min.png';
+
+export const heroData = [
+    {
+        title: "Hell's Paradise",
+        image: hell,
+        synopsis: 'Set in the Edo period of Japan, it follows the ninja Gabimaru and the executioner Yamada Asaemon Sagiri as they search for the elixir of immortality.'
+    },
+    {
+        title: 'Jujutsu Kaisen',
+        image: jjk,
+        synopsis: 'Satoru Gojo and Suguru Geto have been assigned a mission to escort the Star Plasma Vessel to Master Tengen.'
+    },
+    {
+        title: 'Rurouni Kenshin',
+        image: kenshin,
+        synopsis: 'Rurouni Kenshin tells the story of a wanderer named Kenshin, who used to be called Hitokiri Battousai during the Japanese civil war.'
+    },
+    {
+        title: 'Bleach TYBW',
+        image: tyb,
+        synopsis: 'Ichigo Kurosaki is a teenager from Karakura Town who can see ghosts, a talent allowing him to meet a supernatural human Rukia Kuchiki, who enters the town in search of a Hollow, a kind of monstrous lost soul who can harm both ghosts and humans.'
+    }
+]
--- src/features/animes/HeroAnimes.jsx
+++ src/features/animes/HeroAnimes.jsx
@@ -1,11 +1,15 @@
 import styles from './HeroAnimes.module.css';
 
+import { HeroBackground } from '../../components/heroBackground/HeroBackground';
+
+import { heroData }  from '../../data/heroData';
 import { useState, useEffect } from 'react';
 
 
 export const HeroAnimes = () => {
 
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentHero, setCurrentHero] = useState();    
 
     useEffect(() => {
 
@@ -14,34 +18,35 @@ export const HeroAnimes = () => {
 
             if (currentIndex === 3 || currentIndex > 3) {
                 setCurrentIndex(0);
+                setCurrentHero(heroData[0]);
                 return;
             }
             setCurrentIndex(prev => prev + 1);
+            setCurrentHero(heroData[currentIndex]);
+
         }, 4000)
     })
 
-    const changeIndex = (index) => {
-        setCurrentIndex(index);
-    }
-
+    // const changeIndex = (index) => {
+    //     setCurrentIndex(index);
+    // }
 
     return (
         <>
             <div className={styles[`backgroundImage${currentIndex}`]}>
                 <div className={styles.content}>
-                    <h2>Hell's Paradise</h2>
-                    <p>Set in the Edo period of Japan, it follows the ninja Gabimaru and the executioner Yamada Asaemon Sagiri as they search for the elixir of immortality.</p>
+                    <h2>{currentHero ? currentHero.title : heroData[0].title}</h2>
+                    <p>{currentHero ? currentHero.synopsis : heroData[0].synopsis}</p>
                     <button>Watch Now</button>
+                    {/* <h1 className={styles.debug}>{currentHero.title}</h1> */}
                 </div>
                 <div className={styles.circleContainer}>
-                    <div className={currentIndex === 0 ? styles.circleActive : styles.circle} onClick={() => changeIndex(0)}></div>
-                    <div className={currentIndex === 1 ? styles.circleActive : styles.circle} onClick={() => changeIndex(1)}></div>
-                    <div className={currentIndex === 2 ? styles.circleActive : styles.circle} onClick={() => changeIndex(2)}></div>
-                    <div className={currentIndex === 3 ? styles.circleActive : styles.circle} onClick={() => changeIndex(3)}></div>
+                    <div className={currentIndex === 0 ? styles.circleActive : styles.circle}></div>
+                    <div className={currentIndex === 1 ? styles.circleActive : styles.circle}></div>
+                    <div className={currentIndex === 2 ? styles.circleActive : styles.circle}></div>
+                    <div className={currentIndex === 3 ? styles.circleActive : styles.circle}></div>
                 </div>
-                <h1 id={styles.debug}>{currentIndex}</h1>
             </div>
-
         </>
     )
 }
