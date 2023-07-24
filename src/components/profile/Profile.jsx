9dc44a6d3efd60a63f7bc7678bad2dd3f65c68c2
JohnnyBoi <ninjakai25@gmail.com>
2023-07-23 19:06:19 -0500

    add state for pfp image and add hover animation on pfp image

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -2,14 +2,20 @@ import styles from './Profile.module.css';
 
 import goku from '../../images/pfp/gokupfp.png';
 
-
 export const Profile = ({ username }) => {
+
+
+
+
 
     return (
         <>
-            <div className={styles.profileContainer}>
+            <div className={styles.profileContainer} >
                 <div className={styles.profileWrapper}>
-                    <img id={styles.pfp} src={goku} />
+                    <div className={styles.pfpImage}>
+                        <img id={styles.pfp} src={goku} />
+                        <p id={styles.changePfp}>Change</p>
+                    </div>
                     <h2>{username}</h2>
                 </div>
             </div>
--- src/features/isLoggedIn/isLoggedInSlice.js
+++ src/features/isLoggedIn/isLoggedInSlice.js
@@ -1,20 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import goku from '../../images/pfp/gokupfp.png';
+
 const isLoggedIn = createSlice({
     name: 'isLoggedIn',
     initialState: {
         loggedIn: false,
-        name: ''
+        name: '',
+        pfp: goku
     },
     reducers: {
         logIn: (state, action) => {
             state.loggedIn = true;
             state.name = action.payload;
+        },
+        changePfp: (state, action) => {
+            state.pfp = action.payload;
         }
     }
 })
 
 export default isLoggedIn.reducer;
 export const { logIn } = isLoggedIn.actions;
+export const { changePfp } = isLoggedIn.actions;
 export const selectIsLoggedIn = state => state.loggedIn.loggedIn;
 export const selectUsername = state => state.loggedIn.name;
+export const selectPfp = state => state.loggedIn.pfp;
