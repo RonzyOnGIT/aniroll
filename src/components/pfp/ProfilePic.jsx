8a8ab8f8ce494a6e54706f45cf93e7c3871a21df
Ronzy <ninjakai25@gmail.com>
2023-07-26 17:18:21 -0500

    adjust hover positioning text in acc page and update pfp state on click

--- src/components/pfp/ProfilePic.jsx
+++ src/components/pfp/ProfilePic.jsx
@@ -1,10 +1,19 @@
 import stlyes from './ProfilePic.module.css';
 
+import { changePfp } from '../../features/isLoggedIn/isLoggedInSlice';
+import { useDispatch } from 'react-redux';
+
 export const ProfilePic = ({ img }) => {
+
+    const dispatch = useDispatch();
+    
+    const updatePfp = () => {
+        dispatch(changePfp(img));
+    }
     
     return(
         <>
-            <img src={img} />
+            <img id={stlyes.image} src={img} onClick={updatePfp} />
         </>
     )
 }
