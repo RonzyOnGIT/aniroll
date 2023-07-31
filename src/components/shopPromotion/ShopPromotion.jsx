7ddc7c300b0538856fb0b0af4c57661a483a202a
JohnnyBoi <ninjakai25@gmail.com>
2023-07-30 21:11:18 -0500

    create banner promo component

--- src/components/shopPromotion/ShopPromotion.jsx
+++ src/components/shopPromotion/ShopPromotion.jsx
@@ -1,12 +1,13 @@
 import styles from './ShopPromotion.module.css';
+import { NavLink } from 'react-router-dom';
 
-export const ShopPromotion = () => {
+export const ShopPromotion = ({ image, navTo }) => {
 
     return(
 
         <>
             <div className={styles.promotionContainer}>
-                <p>Shop Sale</p>
+                <NavLink to={navTo}><img src={image} /></NavLink>
             </div>
         </>
     )
