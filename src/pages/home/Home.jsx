8c4492615bf6bbbac9f565ab00e85498da6c0629
Ronzy <ninjakai25@gmail.com>
2023-08-01 13:17:11 -0500

    Revert "Revert "create banner promo component""
    This reverts commit 19e0cdedb79c332a24776113ec844b4f35ede3a2.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,8 @@ import styles from './Home.module.css';
 
 import { Hero } from '../../components/hero/Hero';
 import { HomeSection } from '../../components/homeSection/HomeSection';
-
-
+import { ShopPromotion } from '../../components/shopPromotion/ShopPromotion';
+import flashSale from '../../images/banners/promoBanner-min.png';
 
 export const Home = () => {
 
@@ -13,6 +13,7 @@ export const Home = () => {
             <div className={styles.homeContainer}>
                 <HomeSection title={'Recommendations'} endPoint={'top/anime?limit=25'} />
                 <HomeSection title={'New'} endPoint={'seasons/now'} />
+                <ShopPromotion image={flashSale} navTo={'store'} />
             </div>
         </>
     )
