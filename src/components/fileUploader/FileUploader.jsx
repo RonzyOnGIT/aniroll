f460684aab446f5be3e27e2748702412a7d2d3be
JohnnyBoi <ninjakai25@gmail.com>
2023-08-10 17:05:53 -0500

    upload custom pfp and updates current one

--- src/components/fileUploader/FileUploader.jsx
+++ src/components/fileUploader/FileUploader.jsx
@@ -1,24 +1,46 @@
 import styles from './FileUploader.module.css';
 import { changePfp } from '../../features/isLoggedIn/isLoggedInSlice';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import axios from 'axios';
 
 export const FileUploader = () => {
 
     const dispatch = useDispatch();
 
-    // const handleFileUpload = (e) => {
-    //     e.preventDefault();
-    //     console.log(e);
-    // }
+    const [customPfp, setCustomPfp] = useState();
+
+
+    const handleFileUpload = (e) => {
+        e.preventDefault();
+        if (!customPfp) {
+            console.log('no file selected');
+            return;
+        }
+
+        const fd = new FormData();
+        fd.append('file', customPfp);
+
+        axios.post('https://httpbin.org/post', fd, {
+            headers: {
+                'Custom-Header': 'value',
+            }
+        })
+        .then(res => dispatch(changePfp(res.data.files.file)))
+        .catch(err => console.error(err));
+        
+    }
+
+
 
     return (
 
         <>
         
-            <form className={styles.fileUploadWrapper} >
-                <input id='file' type='file' accept='image/*' />
+            <form className={styles.fileUploadWrapper} onSubmit={handleFileUpload}>
+                <input id='file' type='file' accept='image/*' onChange={(e) => setCustomPfp(e.target.files[0])} />
                 <label htmlFor='file'>Upload Custom Picture</label>
-                <button type='submit'>Submit</button>
+                <button type='submit'>Update Pfp</button>
             </form>
         </>
     )
