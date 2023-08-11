247fca5defa156e59bb5e4dc763eb86389b92197
JohnnyBoi <ninjakai25@gmail.com>
2023-08-11 15:02:16 -0500

    add preview for custom pfp and add loading page to account page

--- src/components/fileUploader/FileUploader.jsx
+++ src/components/fileUploader/FileUploader.jsx
@@ -10,6 +10,7 @@ export const FileUploader = () => {
 
     const [customPfp, setCustomPfp] = useState();
     const [noFileChosen, setNoFileChosen] = useState(false);
+    const [previewPfp, setPreviewPfp] = useState();
 
 
     const handleFileUpload = (e) => {
@@ -20,19 +21,26 @@ export const FileUploader = () => {
             setNoFileChosen(true);
             return;
         }
-        setNoFileChosen(false);
+        setNoFileChosen(false);  
 
         const fd = new FormData();
         fd.append('file', customPfp);
+        setPreviewPfp();  
 
         axios.post('https://httpbin.org/post', fd, {
             headers: {
                 'Custom-Header': 'value',
             }
         })
-        .then(res => dispatch(changePfp(res.data.files.file)))
+        .then(res =>{
+            dispatch(changePfp(res.data.files.file))})
         .catch(err => console.error(err));
         
+    }
+
+    const handleFileChange = (e) => {
+        setCustomPfp(e.target.files[0]);
+        setPreviewPfp(URL.createObjectURL(e.target.files[0]));  
     }
 
 
@@ -42,10 +50,11 @@ export const FileUploader = () => {
         <>
         
             <form className={styles.fileUploadWrapper} onSubmit={handleFileUpload}>
-                <input id='file' type='file' accept='image/*' onChange={(e) => setCustomPfp(e.target.files[0])} />
+                <input id='file' type='file' accept='image/*' onChange={handleFileChange} />
                 <label htmlFor='file'>Upload Custom Picture</label>
                 <button type='submit'>Update pfp</button>
                 {noFileChosen && <p>No File Chosen</p>}
+                {previewPfp && <img id={styles.pfpPreview} src={previewPfp} />}
             </form>
         </>
     )
--- src/data/pfpData.js
+++ src/data/pfpData.js
@@ -2,6 +2,8 @@ import goku from '../images/pfp/gokupfp.png';
 import naruto from '../images/pfp/narutopfp-min.png';
 import tanjiro from '../images/pfp/tanjiropfp-min.png';
 import aqua from '../images/pfp/aquapfp-min.png';
+import saitama from '../images/pfp/saitama-min.png';
+import kagome from '../images/pfp/kagome-min.png';
+import asuna from '../images/pfp/asuna-min.png';
 
-
-export const pfpImages = [goku, naruto, tanjiro, aqua];
+export const pfpImages = [goku, naruto, tanjiro, aqua, saitama, kagome, asuna];
