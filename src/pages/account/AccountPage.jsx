d27577ebdcaea23e7171f0872c5a0833dba3f6e3
JohnnyBoi <ninjakai25@gmail.com>
2023-08-11 09:48:26 -0500

    align button center, add loading while background loads in acc page, and vertically center loader

--- src/pages/account/AccountPage.jsx
+++ src/pages/account/AccountPage.jsx
@@ -4,25 +4,39 @@ import { AccountForm } from '../../components/form/AccountForm';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn, selectUsername } from '../../features/isLoggedIn/isLoggedInSlice';
 import { Profile } from '../../components/profile/Profile';
+import { useState } from 'react';
+import { Loading } from '../../components/loading/Loading';
+
+import backgroundTest from '../../images/backgrounds/accBackground-min.png';
 
 export const AccountPage = () => {
 
     const isLoggedIn = useSelector(selectIsLoggedIn);
     const username = useSelector(selectUsername);
 
+    const [hasBackgroundLoaded, setHasBackgroundLoaded] = useState(false);
+
+    const handleImageHasLoaded = () => {
+        setHasBackgroundLoaded(true);
+    }
+
     return (
         <>
+            {/* use same background img from css to to wait to show page until image has loaded */}
+            <img id={styles.backgroundForLoading} src={backgroundTest} onLoad={handleImageHasLoaded} />
 
-            <div className={styles.accountPageContainer}>
-                {isLoggedIn 
-                    ?   <Profile username={username} />
-                    :   <div className={styles.notLoggedInContainer}>
+            {!hasBackgroundLoaded
+            
+             ? <Loading />
+             : <div className={styles.accountPageContainer}>
+                    {isLoggedIn 
+                     ?  <Profile username={username} />
+                     :  <div className={styles.notLoggedInContainer}>
                             <h2>Create new account</h2>
                             <AccountForm />
                         </div> 
-                }
-            </div>
-            
+                    }
+                </div> }     
         </>
     )
 }
