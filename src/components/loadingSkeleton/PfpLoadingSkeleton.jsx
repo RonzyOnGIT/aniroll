7599950f347e05125a7a7167eeeaf64513ab1f65
JohnnyBoi <ninjakai25@gmail.com>
2023-08-13 15:06:34 -0500

    create and add component for loading skeleton for pfp

---
+++ src/components/loadingSkeleton/PfpLoadingSkeleton.jsx
@@ -0,0 +1,10 @@
+import styles from './PfpLoadingSkeleton.module.css';
+
+export const PfpLoadingSkeleton = () => {
+
+    return (
+        
+        <div className={styles.pfpSkeleton}>
+        </div>
+    )
+}
--- src/components/pfp/ProfilePic.jsx
+++ src/components/pfp/ProfilePic.jsx
@@ -2,18 +2,27 @@ import stlyes from './ProfilePic.module.css';
 
 import { changePfp } from '../../features/isLoggedIn/isLoggedInSlice';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { PfpLoadingSkeleton } from '../loadingSkeleton/PfpLoadingSkeleton';
+
 
 export const ProfilePic = ({ img }) => {
 
     const dispatch = useDispatch();
+    const [isImageLoading, setIsImageLoading] = useState(true);
     
+    const handleImageLoad = () => {
+        setIsImageLoading(false);
+    }
+
     const updatePfp = () => {
         dispatch(changePfp(img));
     }
     
     return(
         <>
-            <img id={stlyes.image} src={img} onClick={updatePfp} />
+            <img id={stlyes.dummy} src={img} onLoad={handleImageLoad} />
+            {isImageLoading ? <PfpLoadingSkeleton /> : <img id={stlyes.image} src={img} onClick={updatePfp} />}
         </>
     )
 }
