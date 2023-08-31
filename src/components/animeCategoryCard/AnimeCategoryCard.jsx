8903ded2ba4438e48d2a884d98716457a1d9044b
JohnnyBoi <ninjakai25@gmail.com>
2023-08-30 20:53:26 -0500

    add loading skeleton to anime categories

--- src/components/animeCategoryCard/AnimeCategoryCard.jsx
+++ src/components/animeCategoryCard/AnimeCategoryCard.jsx
@@ -1,13 +1,21 @@
 import styles from './AnimeCategoryCard.module.css';
+import { useState } from 'react';
 
 export const AnimeCategoryCard = ({ category, cover }) => {
+
+    const [hasImageLoaded, setHasImageLoaded] = useState(false);
+
+    const imageLoaded = () => {
+        setHasImageLoaded(true);
+    }
 
     return (
 
         <>
+            <img onLoad={imageLoaded} id={styles.imagePlaceholder} src={cover} />
             <div className={styles.categoryCardContainer}>
                 <img id={styles.cover} src={cover} />
-                <p id={styles.categoryHeader}>{category}</p>
+                <p className={hasImageLoaded ? styles.categoryHeader : `${styles.categoryHeader} ${styles.skeleton}`}>{category}</p>
             </div>
         </>
     )
