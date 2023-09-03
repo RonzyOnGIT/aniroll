22025d5b44bd455276b27a8d0751bd1bf8d7ea74
JohnnyBoi <ninjakai25@gmail.com>
2023-09-03 16:09:26 -0500

    add go back button in shop category page and fix page refresh

--- src/pages/categories/CategoriesItems.jsx
+++ src/pages/categories/CategoriesItems.jsx
@@ -1,9 +1,11 @@
 import styles from './CategoriesItems.module.css';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export const CategoriesItems = () => {
 
     const { category } = useParams();
+    const navigate = useNavigate();
 
     return (
 
@@ -11,6 +13,7 @@ export const CategoriesItems = () => {
             <div className={styles.categoriesItemsContainer}>
                 <h1>Shop {category}</h1>
             </div>
+            <button id={styles.backButton} onClick={() => navigate('/store')}>Go Back</button>
         </>
     )
 }
