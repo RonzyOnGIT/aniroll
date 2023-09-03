a8bf65f44f100d9c8df18ff036ebfb6991bcb565
JohnnyBoi <ninjakai25@gmail.com>
2023-09-03 10:50:41 -0500

    add loading screen to shop

--- src/components/shopCategoryCard/ShopCategoryCards.jsx
+++ src/components/shopCategoryCard/ShopCategoryCards.jsx
@@ -1,15 +1,30 @@
 import styles from './ShopCategoryCards.module.css';
 import {categoryItems} from '../../data/shopData';
 import { ShopCategoryCard } from '../../components/shopCategoryCard/ShopCategoryCard';
+import { useState } from 'react';
+import gokuPlaceholder from '../../images/backgrounds/0821_NSF.webp';
+import { Loading } from '../loading/Loading';
 
 export const ShopCategoryCards = () => {
+
+    const [hasImageLoaded, setHasImageLoaded] = useState();
+
+    const imageLoaded = () => {
+        setHasImageLoaded(true);
+    }
 
     return (
 
         <>
-            <div className={styles.categoriesContainer}>
-                {categoryItems.map(category => <ShopCategoryCard image={category.image} category={category.category} key={category.category} />)}
-            </div>
+
+            <img id={styles.placeholder} src={gokuPlaceholder} onLoad={imageLoaded} />
+            {hasImageLoaded
+             ?  <div className={styles.categoriesContainer}>
+                    {categoryItems.map((category, index) => <ShopCategoryCard image={category.image} category={category.category} key={index} />)}
+                </div>
+             : <Loading />
+            }
+
         </>
     )
 }
